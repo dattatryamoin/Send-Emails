1a8ab7d56c988570dbc2f2fe3b5761a727bc5547
dattatraymoin <dattatryamoin@gmail.com>
2017-03-23 18:01:26 +0530

    Send-Emails

---
+++ Emails/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Emails.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult SendEmailView()
+        {
+            //call SendEmailView view to invoke webmail  
+            return View();
+        }  
+    }
+}
